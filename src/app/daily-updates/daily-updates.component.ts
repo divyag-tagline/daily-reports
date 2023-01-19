19051c034bb1f214cd4ab6eb2d93cd4addeba4a3
divyag_tagline <divyag.tagline@gmail.com>
2023-01-19 15:02:10 +0530

    Complete  formArray with validation in CompleteTask

--- src/app/daily-updates/daily-updates.component.ts
+++ src/app/daily-updates/daily-updates.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-daily-updates',
@@ -8,25 +14,47 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DailyUpdatesComponent implements OnInit {
   dailyUpdates!: FormGroup;
-  complateTask!: FormArray;
+  completeTask!: FormArray;
+  completedTask : any
+  toggle: boolean = false;
+  subbmitted!: boolean;
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.dailyUpdates = this.formBuilder.group({
       clientName: ['', Validators.required],
       projectName: ['', Validators.required],
-      complateTask: this.formBuilder.array([this.formBuilder.control('')]),
+      completeTask: this.formBuilder.array([this.completeing()]),
     });
-    console.log(this.dailyUpdates.controls);
   }
-
+  
   get dailyUpdatesControls() {
     return this.dailyUpdates.controls;
   }
   get completeTasks() {
-    return this.dailyUpdates.get('complateTask') as FormArray;
+    return this.dailyUpdates.get('completeTask') as FormArray;
   }
-  onAddTaks() {
-    this.completeTasks.push(this.formBuilder.control(''));
+  private completeing(): FormGroup {
+    return new FormGroup({
+      completeingTask: new FormControl('', [Validators.required]),
+    });
+  }
+  public onAddTaks() {
+    this.completeTasks.push(this.completeing());
+    this.toggle = true;
+    this.subbmitted = false
+    console.log(this.completeTasks.value);
+  }
+  completeTaskValidation(index:number) {
+    this.completedTask = this.dailyUpdates.get('completeTask') as FormArray; 
+    const formGroup = this.completedTask.controls[index] as FormGroup;
+    return formGroup;
+  }
+  public removeOrClearCompletingTask(i: number) {
+    if (this.completeTasks.length > 1) {
+      this.completeTasks.removeAt(i)
+    } else {
+      this.completeTasks.reset()
+    }
   }
 }
