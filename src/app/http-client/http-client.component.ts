e31b9719d5ab30d3a36c787be3166006778b6655
divyag_tagline <divyag.tagline@gmail.com>
2023-01-23 16:56:23 +0530

    Add get api in httpclient

--- src/app/http-client/http-client.component.ts
+++ src/app/http-client/http-client.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-http-client',
@@ -7,16 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./http-client.component.scss'],
 })
 export class HttpClientComponent implements OnInit {
-  usersData !: any;
-  constructor(private http: HttpClient) {
-    this.http
-      .get('https://jsonplaceholder.typicode.com/users')
-      .subscribe((data) => {
-        this.usersData = data;
-        console.log(this.usersData);
-      });
-      
+  usersData!: any;
+  user: any;
+  userDetails!: FormGroup;
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.userDetails = this.formBuilder.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
+    this.http.get(`${environment.baseURL}/users`).subscribe((data) => {
+      this.usersData = data;
+      console.log(this.usersData);
+    });
   }
 
   ngOnInit(): void {}
+
+  addUser(): any {
+    let details = {
+      id: +this.usersData.length + 1,
+      ...this.userDetails.value,
+    };
+    console.log(details);
+    this.http
+      .post(`${environment.baseURL}/users`, details)
+      .subscribe((data) => {
+        this.usersData.push(data);
+      });
+    this.userDetails.reset();
+  }
 }
